242e1403340cad9d3d10d3c6dfbb805e01005c65
Renan Bastos <renan.bastos@crea-rj.org.br>
2017-02-15 16:27:10 -0200

    fix layout

--- index.js
+++ index.js
@@ -5,33 +5,20 @@ var io = require('socket.io')(http);
 var filaDoDia = [];
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index_layout.html');
 });
 
 app.get('/guiche', function(req, res){
   res.sendFile(__dirname + '/guiche.html');
 });
 
-// unshift/push - add an element to the beginning/end of an array
-// shift/pop - remove and return the first/last element of and array
 
 io.on('connection', function(socket){
 
   socket.on('painelCrea', function(msg){
 
-
     filaDoDia.unshift(msg);
     io.emit('painelCrea', filaDoDia);
-
-    // filaDoDia.forEach(function(el){
-    //   if(msg === el){
-    //     console.log(el);
-    //   }
-    // });
-
-    // if(filaDoDia.length > 1){
-    //   io.emit('anteriores', filaDoDia);
-    // }
 
     if(filaDoDia.length > 4){
       filaDoDia.pop();
